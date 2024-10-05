2032099c72aeb101d23d5be396a7ff70163f9566
SebaParraa <sparraf@usm.cl>
2024-10-05 20:43:19 -0300

    Task pending

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,7 @@ from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import  TaskForm
 from .models import Task
+from django.utils import timezone
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -70,6 +71,13 @@ def task_detail(request, task_id):
             return redirect('tasks')
         except ValueError:
             return render(request, 'task_detail.html', {'task': task, 'form':form, 'error': "Error updating task"}) 
+def complete_task(request, task_id):
+    task = get_object_or_404(Task,pk=task_id, user=request.user)
+    if request.method =='POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+
 def signout(request):
     logout(request)   
     return redirect('home')
