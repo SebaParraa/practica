e1b21d10a905f72dc41659353052c3a784adbd45
SebaParraa <sparraf@usm.cl>
2024-10-05 21:14:06 -0300

    Proteger Rutas

--- tasks/views.py
+++ tasks/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login, logout, authenticate
 from .forms import  TaskForm
 from .models import Task
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -33,13 +34,17 @@ def signup(request):
                     'form': UserCreationForm,
                     'error': 'Password do not match' 
         })   
+@login_required
 def tasks (request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request,'tasks.html', {'tasks': tasks})    
+
+@login_required
 def tasks_completed (request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
     return render(request,'tasks.html', {'tasks': tasks})    
 
+@login_required
 def create_task(request):
 
     if request.method == 'GET':
@@ -59,6 +64,7 @@ def create_task(request):
                 'error': 'Please provide valida data'
     })
 
+@login_required
 def task_detail(request, task_id):
     if request.method == 'GET':
         task = get_object_or_404(Task, pk=task_id, user= request.user)
@@ -72,17 +78,23 @@ def task_detail(request, task_id):
             return redirect('tasks')
         except ValueError:
             return render(request, 'task_detail.html', {'task': task, 'form':form, 'error': "Error updating task"}) 
+        
+@login_required
 def complete_task(request, task_id):
     task = get_object_or_404(Task,pk=task_id, user=request.user)
     if request.method =='POST':
         task.datecompleted = timezone.now()
         task.save()
         return redirect('tasks')
+    
+@login_required    
 def delete_task(request, task_id):
     task = get_object_or_404(Task,pk=task_id, user=request.user)
     if request.method =='POST':
         task.delete()
         return redirect('tasks')
+    
+@login_required
 def signout(request):
     logout(request)   
     return redirect('home')
